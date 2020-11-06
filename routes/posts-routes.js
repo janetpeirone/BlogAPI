be3177f650e32ebb99f65a635a5e62d9dbcdcf0e
Janet Peirone <janet.peirone@gmail.com>
2020-11-05 21:58:24 -0300

    update post router added

--- routes/posts-routes.js
+++ routes/posts-routes.js
@@ -70,4 +70,68 @@ router.delete('/posts/:id', (req,res) => {
     })
 })
 
+// Update post by id
+router.patch('/posts/:id', (req,res) => {  
+    // Check if req.body is empty and send msg 
+    if(Object.keys(req.body).length === 0) {
+        res.json('No changes to do')
+    } 
+
+    else {
+        // Check if category is defined
+        if (req.body.category === undefined) {            
+            // If category is not defined, only update posts table        
+            Post.update( req.body , {
+                where: { id: req.params.id }
+            })
+            .then( ([post]) => {
+                if(post === 0) {
+                    res.json('Error: Post not found!');        
+                }
+                else {
+                    res.json('Post was just updated');
+                }
+            })
+            .catch( (error) => {
+                res.json(error)
+            })
+        }
+    
+        else {
+            // If category is not null, first find or create in categories table
+            Category.findOrCreate({
+                where: { category: req.body.category }
+            })
+            .then( ([category, created]) => {  
+                // then update posts table      
+                Post.update( req.body , {
+                    where: { id: req.params.id }
+                })
+                .then((updated) => {
+                    // then find the post updated to set the category               
+                    Post.findByPk(req.params.id)
+                    .then( (post) => {
+                        if(post === null) {
+                            res.json('Error: Post not found!');        
+                        }
+                        else {
+                            post.setCategory(category);
+                            res.json(post);
+                        }                        
+                    })
+                    .catch( (error) => {
+                        res.json(error)
+                    })               
+                })
+                .catch( (error) => {
+                    res.json(error)
+                })             
+            })
+            .catch( (error) => {
+                res.json(error)
+            })   
+        }    
+    }    
+})
+
 module.exports = router;
